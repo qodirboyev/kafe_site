f14afb71d082a7e1f83f1205f8e07864037072a5
DEMIR <qadirboevdemir@gmail.com>
2026-08-19 21:22:58 +0500

    add static files

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 
