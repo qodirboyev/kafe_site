036efb8233d043778f4781b1430b2eac64b369e6
DEMIR <qadirboevdemir@gmail.com>
2026-08-23 18:05:27 +0500

    allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = os.getenv("*")
 
 
 # Application definition
